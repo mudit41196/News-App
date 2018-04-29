fb79f982cd99182d1d084a1d3be89e7256de1f4e
m0a00tt <mudit.arora@walmartlabs.com>
2018-04-29 19:04:42 +0530

    color formatting

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,97 +1,104 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, ActivityIndicator, View, Alert, FlatList,TouchableOpacity, Image} from 'react-native';
+import { StyleSheet, Text, ActivityIndicator, View, Alert, FlatList, TouchableOpacity, Image } from 'react-native';
 import TimeAgo from '../Common/time';
 import { connect } from 'react-redux';
 
 class HomeScreen extends Component {
-  constructor(props)
-  {
-      super(props);
-      this.state = {
-          selected: [],
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: [],
+    };
   };
   componentDidMount() {
-    const {getNews} = this.props;
+    const { getNews } = this.props;
     getNews();
   }
-  check(words)
-  {
-      if(words==null)
-      {
-          return ' ';
-      }
-      return words.toString();
+  check(words) {
+    if (words == null) {
+      return ' ';
+    }
+    return words.toString();
   };
-  open(ur)
-  {
-      Linking.canOpenURL(ur.toString()).then(supported=>
-      {
-          if(supported)
-          {
-              Linking.openURL(ur.toString());
-          }  
-          else
-          {
-              alert('Error: cannot open this url');
-          } 
-      });
+  open(ur) {
+    Linking.canOpenURL(ur.toString()).then(supported => {
+      if (supported) {
+        Linking.openURL(ur.toString());
+      }
+      else {
+        alert('Error: cannot open this url');
+      }
+    });
   };
   changeState(index) {
-      const { selected } = this.state;
-      const i = selected.indexOf(index);
-      if(i > -1){
-          selected.splice(i,1);
-      } else {
-          selected.push(index);
-      }
-      console.log(selected);
-      this.setState({ selected });
+    const { selected } = this.state;
+    const i = selected.indexOf(index);
+    if (i > -1) {
+      selected.splice(i, 1);
+    } else {
+      selected.push(index);
+    }
+    console.log(selected);
+    this.setState({ selected });
   }
-  displayList(data)
-  {   
-      const { item, index } = data;
-      const { urlToImage, description, source, publishedAt, title, url } = item;
-      const { selected } = this.state;
-      const i = selected.indexOf(index);
-      const details = (i > -1) ? (
+  displayList(data) {
+    const { item, index } = data;
+    const { urlToImage, description, source, publishedAt, title, url } = item;
+    const { selected } = this.state;
+    const i = selected.indexOf(index);
+    const details = (i > -1) ? (
       <View>
-      <Text>{this.check(description)} </Text>
-      <Text note>{this.check(source.name)} </Text>
-      <TimeAgo date={publishedAt} />
-      </View>) : (<Text/>);
-      return(
-          <TouchableOpacity style={{ flex: 1,flexDirection:'row', padding: 10, borderColor:'#979797', borderBottomWidth: 0.5}} activeOpacity={0.5} onPress={()=> this.changeState(index)}>
-              <View style={{flex: 2}}>
-                  <Image style={{width: 50, height: 50, alignSelf:'center'}} source={{uri:urlToImage!=null?this.check(urlToImage): 'https://images.pexels.com/photos/789141/pexels-photo-789141.jpeg?h=350&auto=compress&cs=tinysrgb' }}/>
-              </View>
-              <View style={{flex: 4, flexWrap:'wrap'}}>
-                  <Text style={{fontSize:16}}>{this.check(title)} </Text>
-                  {details}
-              </View>
-          </TouchableOpacity>
-      );
-  }
-         
-    render(){
-      console.log(this.props.newsHeadlines);
-      const ViewGeneric = this.props.isLoading ? (
-        <View style={{flex: 1, alignItems:'center', justifyContent:'center'}}>
-          <ActivityIndicator animating={this.props.isLoading} animating={this.props.isLoading}/>
-          <Text>Please Wait </Text>
+        <Text>{this.check(description)} </Text>
+        <Text note>{this.check(source.name)} </Text>
+          <View style={{alignItems: 'flex-end', marginRight: 5}} >
+            <TimeAgo date={publishedAt} />
+          </View>
+      </View>) : (<Text />);
+    const timeDetails = (i > -1) ? (<Text />) : (<TimeAgo date={publishedAt} />);
+    return (
+      <TouchableOpacity style={{ backgroundColor: "#fff4d8", flex: 1, flexDirection: 'row', borderColor: '#2d9961', borderBottomWidth: 1, padding: 5 }} activeOpacity={0.5} onPress={() => this.changeState(index)}>
+        <View style={{ flex: 3, justifyContent: "flex-start" }}>
+          <Image style={{ width: 100, height: 120, resizeMode: 'stretch' }} source={{ uri: urlToImage != null ? this.check(urlToImage) : 'https://images.pexels.com/photos/789141/pexels-photo-789141.jpeg?h=350&auto=compress&cs=tinysrgb' }} />
         </View>
-      ) : ( <FlatList
-        data={this.props.newsHeadlines}
-        renderItem={(item)=> this.displayList(item)}
-        keyExtractor={(item)=> item.url}
-        extraData={this.state}
-        />
+        <View style={{ flex: 5, flexWrap: 'wrap' }}>
+          <Text style={{ fontSize: 16, color: "#2d9961" }}>{this.check(title)} </Text>
+          <View style={{ alignItems: 'flex-end', marginRight: 5 }}>
+            {timeDetails}
+          </View>
+          {details}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    console.log(this.props.newsHeadlines);
+    const {newsHeadlines} = this.props;
+    let length=0;
+    if(newsHeadlines){length = newsHeadlines.length};
+    const ViewGeneric = this.props.isLoading ? (
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <ActivityIndicator animating={this.props.isLoading} animating={this.props.isLoading} />
+        <Text>Please Wait </Text>
+      </View>
+    ) : (
+          <FlatList
+            data={this.props.newsHeadlines}
+            renderItem={(item) => this.displayList(item)}
+            keyExtractor={(item) => item.url}
+            extraData={this.state}
+          />
       );
-      return (
-        <View style={{flex: 1}}>
+    return (
+      <View style={{flex: 1, backgroundColor:"#ffff"}}>
+        <View style={{margin: 10}}>
+          <Text style={{color: "#2d9961", fontSize: 25, fontWeight: "bold"}}> All categories ({length})  </Text>
+        </View>
+        <View style={{marginHorizontal: 10, marginVertical: 25}}>
           {ViewGeneric}
         </View>
-      );
+      </View>
+    );
   }
 
 }
@@ -99,26 +106,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#26633e',
-    paddingLeft:10,
-    paddingRight:40,
-    justifyContent:'flex-end',
-    flexDirection:'row',
+    paddingLeft: 10,
+    paddingRight: 40,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
   },
-  header:{
-    fontWeight:'bold',
-    fontSize:15,
-    color:'white',
+  header: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: 'white',
   },
-  touch:{
-    flex:1,
-    padding:10,
-    justifyContent:'flex-end',
+  touch: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'flex-end',
     alignItems: 'center',
   }
 
 });
 
-const mapStateToProps = ({news}) => {
+const mapStateToProps = ({ news }) => {
   const {
     newsheadlines, loading
   } = news;
@@ -127,17 +134,17 @@ const mapStateToProps = ({news}) => {
     isLoading: loading
   }
 }
-const mapDispatchToProps =(dispatch)=> {
-  return{
-    getNews : () => {
-    dispatch({
-      type: 'FETCH_NEWS',
-    });
-  },
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getNews: () => {
+      dispatch({
+        type: 'FETCH_NEWS',
+      });
+    },
   };
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(HomeScreen);
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
 
 
 
